651d5e87a80c9e2da73c1cff62fac97baa5ccdf6
Scottsdaaale <scottsdaaale@gmail.com>
2024-02-24 17:38:13 -0500

    added file loader

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    webpack: (config) => {
+      // Add a rule to handle PDF files
+      config.module.rules.push({
+        test: /\.pdf$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[ext]',
+          },
+        },
+      });
+  
+      return config;
+    },
+  };
+  
+  export default nextConfig;
