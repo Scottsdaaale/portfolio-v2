cf94fbd5a5632ce8b97aff34eff47eea714658fe
Scottsdaaale <scottsdaaale@gmail.com>
2024-03-25 18:19:21 -0400

    removed fadeinwrapper added framer motion

--- app/components/routes/dev/Main.js
+++ app/components/routes/dev/Main.js
@@ -1,27 +1,26 @@
+'use client';
 import React from 'react';
 import About from './About';
 import Contact from './Contact';
 import Experience from './Experience';
 import Intro from './Intro';
 import Projects from './Projects';
-import FadeInWrapper from '../../common/FadeInWrapper';
+import { motion } from 'framer-motion';
 
 function Main() {
   return (
-    <div className="px-2 mx-auto max-w-[500px]">
-      <Intro />
-      <FadeInWrapper delay={5000}>
+    <div className='px-2 mx-auto max-w-[500px]'>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.01, duration: 0.5 }}
+      >
+        <Intro />
         <About />
-      </FadeInWrapper>
-      <FadeInWrapper delay={5200}>
         <Experience />
-      </FadeInWrapper>
-      <FadeInWrapper delay={5400}>
         <Projects />
-      </FadeInWrapper>
-      <FadeInWrapper delay={5600}>
         <Contact />
-      </FadeInWrapper>
+      </motion.div>
     </div>
   );
 }
