1144823d1fbaedd97e3f2ca12c76c22a0ec90a45
Scottsdaaale <scottsdaaale@gmail.com>
2024-03-25 17:59:02 -0400

    added framer animations and mobile opacity switch on scroll

--- app/components/global/Header.js
+++ app/components/global/Header.js
@@ -1,101 +1,82 @@
 'use client';
+
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-
-import { Tooltip } from 'react-tooltip';
+import { motion } from 'framer-motion';
 
 import computerGif from '../../assets/gifs/Computer.gif';
 import bookGif from '../../assets/gifs/Book.gif';
 import externalLinkGif from '../../assets/gifs/ExternalLink.gif';
-import toolGif from '../../assets/gifs/Tool2.gif';
-import FadeInWrapper from '../common/FadeInWrapper';
 
 const Header = () => {
-  const [opacity, setOpacity] = useState(1);
+  const [scrolling, setScrolling] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
-      // Check if the screen width is less than 768 pixels
-      const isSmallScreen = window.innerWidth < 768;
-      // Update opacity based on scroll position and screen width
-      setOpacity(isSmallScreen && window.scrollY > 0 ? 0 : 1);
+      if (window.innerWidth <= 768) {
+        if (window.scrollY > 0) {
+          setScrolling(true);
+        } else {
+          setScrolling(false);
+        }
+      }
     };
 
-    // Listen for scroll events
     window.addEventListener('scroll', handleScroll);
 
-    // Cleanup listener on component unmount
-    return () => window.removeEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
   }, []);
 
   return (
-    <div className='sticky top-0 flex justify-end items-center p-4 mx-5' style={{ opacity, transition: 'opacity 0.5s ease' }}>
-      <div className='flex gap-3'>
-        <FadeInWrapper delay={5000}>
+    <div
+      className={`sticky top-0 flex justify-end items-center p-4 mx-5 transition-opacity duration-500 ${
+        scrolling ? 'opacity-50' : 'opacity-100'
+      }`}
+    >
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.01, duration: 0.5 }}
+      >
+        <div className='flex gap-3'>
           <Link href='/'>
-            <Image
-              src={computerGif}
-              alt='Portfolio Page'
-              width='40'
-              height='40'
-              className=''
-              data-tooltip-id='my-tooltip'
-              data-tooltip-content='Portfolio'
-            />
+            <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+              <Image
+                src={computerGif}
+                alt='Portfolio Page'
+                width='40'
+                height='40'
+                className=''
+              />
+            </motion.div>
           </Link>
-        </FadeInWrapper>
-        {/* <FadeInWrapper delay={5300}>
-          <Link
-            href='/sandbox'
-            data-tooltip-id='my-tooltip'
-            data-tooltip-content='Sandbox'
-          >
-            <Image
-              src={toolGif}
-              alt='Sandbox Page'
-              width='40'
-              height='40'
-              className=''
-            />
+          <Link href='/blog'>
+            <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+              <Image
+                src={bookGif}
+                alt='Blog Page'
+                width='40'
+                height='40'
+                className=''
+              />
+            </motion.div>
           </Link>
-        </FadeInWrapper> */}
-        <FadeInWrapper delay={5300}>
-          <Link
-            href='/blog'
-            data-tooltip-id='my-tooltip'
-            data-tooltip-content='Blog'
-            place='right'
-          >
-            <Image
-              src={bookGif}
-              alt='Blog Page'
-              width='40'
-              height='40'
-              className=''
-            />
+          <Link href='https://linktr.ee/scottsdaaale' target='blank'>
+            <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+              <Image
+                src={externalLinkGif}
+                alt='Link Tree'
+                width='40'
+                height='40'
+                className=''
+              />
+            </motion.div>
           </Link>
-        </FadeInWrapper>
-        <FadeInWrapper delay={5600}>
-          <Link
-            href='https://linktr.ee/scottsdaaale'
-            data-tooltip-id='my-tooltip'
-            data-tooltip-content='LinkTree'
-            place='bottom'
-            target='blank'
-          >
-            <Image
-              src={externalLinkGif}
-              alt='Link Tree'
-              width='40'
-              height='40'
-              className=''
-              
-            />
-          </Link>
-        </FadeInWrapper>
-        <Tooltip id='my-tooltip' place="bottom" />
-      </div>
+        </div>
+      </motion.div>
     </div>
   );
 };
