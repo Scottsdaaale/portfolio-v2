452db42c30dac8e907a9b56c25055e29923a0b01
Scottsdaaale <scottsdaaale@gmail.com>
2024-03-25 18:19:05 -0400

    removed fadeinwrapper

--- app/components/routes/dev/Intro.js
+++ app/components/routes/dev/Intro.js
@@ -8,22 +8,22 @@ function Intro() {
     <div className='mb-14 '>
       <div className='flex flex-row items-center justify-center'>
         <div className=''>
-          <FadeInWrapper delay={0}>
+          {/* <FadeInWrapper delay={0}> */}
             <Image src={logoImage} alt='Logo' className='rounded-full max-w-[100px]' />
-          </FadeInWrapper>
+          {/* </FadeInWrapper> */}
         </div>
         <div className='px-3 flex flex-col'>
-          <FadeInWrapper delay={1800}>
+          {/* <FadeInWrapper delay={1800}> */}
             <h1 className='font-semibold'>
               Hello, my name is Scotty&nbsp;Peterson...
             </h1>
             <h1 className='mb-2'></h1>
-          </FadeInWrapper>
-          <FadeInWrapper delay={3000}>
+          {/* </FadeInWrapper> */}
+          {/* <FadeInWrapper delay={3000}> */}
             <p className=''>
               Developer, Musician, Plant and&nbsp;Cat&nbsp;Dad
             </p>
-          </FadeInWrapper>
+          {/* </FadeInWrapper> */}
         </div>
       </div>
     </div>
