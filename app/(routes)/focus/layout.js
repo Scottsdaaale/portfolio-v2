e7d0849f55465aa74c70701899f5349189c3bfa3
Scottsdaaale <scottsdaaale@gmail.com>
2024-03-28 19:46:52 -0400

    removed header import

--- app/(routes)/focus/layout.js
+++ app/(routes)/focus/layout.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import Header from '../../components/global/Header';
-
 function layout({ children }) {
   return <div className='px-2 xs:px-0 mx-auto max-w-[500px]'>{children}</div>;
 }
