42d87b5b75c04b6262e1d8b2be38285ff8d57524
Scottsdaaale <scottsdaaale@gmail.com>
2024-06-28 19:09:43 -0400

    added tags and description changes

--- app/components/routes/dev/Projects.js
+++ app/components/routes/dev/Projects.js
@@ -8,20 +8,24 @@ import hypertyperGif from '../../../assets/gifs/HyperTyper.gif';
 import woolieGif from '../../../assets/gifs/Woolie.gif';
 import ytpGif from '../../../assets/gifs/YTP.gif';
 
+import Tags from '../../common/Tags';
 
-function Project({ title, description, gif, link }) {
+function Project({ title, description, gif, link, tags }) {
   return (
-    <div className='flex flex-row mb-3 items-center'>
-      <div className='px-5'>
-        <Link href={link} target='_blank' rel='noopener noreferrer'>
-          <h3 className='inline-block hover:text-blue-500 transition ease-in-out mb-2'>
-            {title}
-          </h3>
-        </Link>
-        <p>{description}</p>
-      </div>
-      <div>
-        <Image src={gif} alt='Project' className='max-w-[70px]' />
+    <div className='flex flex-col px-5 mb-8'>
+      <Link href={link} target='_blank' rel='noopener noreferrer'>
+        <h3 className='inline-block hover:text-blue-500 transition ease-in-out mb-2'>
+          {title}
+        </h3>
+      </Link>
+      <div className='flex flex-row'>
+        <div>
+          <p>{description}</p>
+          <Tags tags={tags} />
+        </div>
+        <div className='flex items-center justify-center'>
+          <Image src={gif} alt='Project' className='max-w-[70px]' />
+        </div>
       </div>
     </div>
   );
@@ -31,22 +35,27 @@ function Projects() {
   const projectInfo = [
     {
       title: 'YTP.com →',
-      description: 'Redesigned and built client brand new marketing site. Increased user engagement and speeds by 300%.',
+      description:
+        'Redesigned and built client brand new marketing site. Increased organic traffic by 120%, doubled WOW retention, and incresed page speeds by 800%.',
       gif: ytpGif,
-      link: '',
+      link: 'https://www.yourtrainingprovider.com/',
+      tags: ['Nextjs', 'Tailwind', 'Flask', 'MySQL', 'AWS', 'Custom CMS'],
     },
     {
       title: 'Woolie.tv →',
-      description: 'Build your own experience relaxation platform. Comfy cozy ambient videos and sounds. Build your perfect scene and chill all day long at woolie.tv.',
+      description:
+        'Built using React with Bootstrap, Woolie is a build-your-own experience relaxation platform. Comfy cozy ambient videos and sounds.',
       gif: woolieGif,
       link: 'https://www.woolie.tv/',
+      tags: ['React', 'Bootstrap'],
     },
     {
       title: 'GenreScope →',
       description:
-        'Allows users to discover new music by searching through thousands of genres. Find artists, videos, songs, and playlists.',
+        'Using React, Django, and the Spotify and Youtube APIS, GenreScope allows users to discover new music by searching through thousands of genres. Find artists, videos, songs, and playlists.',
       gif: genrescopeGif,
       link: 'https://github.com/Scottsdaaale/GenreScope',
+      tags: ['React', 'Django', 'Bootstrap', 'Spotify API', 'Youtube API'],
     },
     {
       title: 'MyClip →',
@@ -54,6 +63,7 @@ function Projects() {
         'A video game clip social media site. Features login, comment, and star rating systems.',
       gif: myclipGif,
       link: 'https://github.com/Scottsdaaale/MyClip',
+      tags: ['React', 'CSS', 'Ruby', 'Rails', 'PostgreSQL'],
     },
     // {
     //   title: 'HyperTyper →',
@@ -74,6 +84,7 @@ function Projects() {
           description={info.description}
           gif={info.gif}
           link={info.link}
+          tags={info.tags}
         />
       ))}
     </div>
