cf620e49f1196d511ea0540fe67f126a6caefb4e
Scottsdaaale <scottsdaaale@gmail.com>
2024-06-30 17:44:53 -0400

    removed dynamic favicon

--- app/components/common/metadata.js
+++ app/components/common/metadata.js
@@ -1,4 +1,5 @@
-import getBlogPost from './getBlogPost';
+
+import getBlogPost from "./getBlogPost";
 
 const cheerio = require('cheerio');
 
@@ -7,7 +8,7 @@ function stripHtml(body) {
 
   for (const block of body) {
     const { children } = block;
-    const blockText = children.map((child) => child.text).join('');
+    const blockText = children.map(child => child.text).join('');
     strippedText += blockText;
   }
 
@@ -23,7 +24,9 @@ function shortenMetaDescription(description) {
   return description;
 }
 
-const fallbackDescription = shortenMetaDescription('');
+const fallbackDescription = shortenMetaDescription(
+  ""
+);
 
 const metadata = async (params) => {
   const { slug } = params;
@@ -37,19 +40,9 @@ const metadata = async (params) => {
     let metaDescription = stripHtml(body);
 
     // Shorten metaDescription to 155 characters
-    metaDescription =
-      shortenMetaDescription(metaDescription) || fallbackDescription;
+    metaDescription = shortenMetaDescription(metaDescription) || fallbackDescription;
 
     const metaImage = mainImage ? mainImage.asset.url : '/fallback-image.jpg';
-
-    // Generate favicon URLs based on the mainImage
-    const faviconUrl = mainImage ? mainImage.asset.url : '/favicon.ico';
-    const shortcutIconUrl = mainImage
-      ? `${mainImage.asset.url}`
-      : '/favicon-16x16.png';
-    const appleTouchIconUrl = mainImage
-      ? `${mainImage.asset.url}`
-      : '/apple-touch-icon.png';
 
     return {
       title: pageTitle,
@@ -58,11 +51,6 @@ const metadata = async (params) => {
         title: pageTitle,
         description: metaDescription,
         images: [metaImage],
-      },
-      icons: {
-        icon: faviconUrl,
-        shortcut: shortcutIconUrl,
-        apple: appleTouchIconUrl,
       },
     };
   } else {
@@ -73,13 +61,99 @@ const metadata = async (params) => {
         description: 'No item found',
         images: ['/fallback-image.jpg'],
       },
-      icons: {
-        icon: '/favicon.ico',
-        shortcut: '/favicon-16x16.png',
-        apple: '/apple-touch-icon.png',
-      },
     };
   }
 };
 
 export default metadata;
+
+
+
+
+//this version of the code dynamically changes the favicon based on blog post
+
+// import getBlogPost from './getBlogPost';
+
+// const cheerio = require('cheerio');
+
+// function stripHtml(body) {
+//   let strippedText = '';
+
+//   for (const block of body) {
+//     const { children } = block;
+//     const blockText = children.map((child) => child.text).join('');
+//     strippedText += blockText;
+//   }
+
+//   return strippedText;
+// }
+
+// // Function to shorten the metaDescription to 155 characters
+// function shortenMetaDescription(description) {
+//   const maxLength = 155;
+//   if (description.length > maxLength) {
+//     return description.slice(0, maxLength).trim() + '...';
+//   }
+//   return description;
+// }
+
+// const fallbackDescription = shortenMetaDescription('');
+
+// const metadata = async (params) => {
+//   const { slug } = params;
+//   const blogPostData = await getBlogPost(slug);
+
+//   if (blogPostData) {
+//     const { title, mainImage, body } = blogPostData;
+//     const pageTitle = title || 'Item Not Found';
+
+//     // Convert HTML description to plain text using cheerio
+//     let metaDescription = stripHtml(body);
+
+//     // Shorten metaDescription to 155 characters
+//     metaDescription =
+//       shortenMetaDescription(metaDescription) || fallbackDescription;
+
+//     const metaImage = mainImage ? mainImage.asset.url : '/fallback-image.jpg';
+
+//     // Generate favicon URLs based on the mainImage
+//     const faviconUrl = mainImage ? mainImage.asset.url : '/favicon.ico';
+//     const shortcutIconUrl = mainImage
+//       ? `${mainImage.asset.url}`
+//       : '/favicon-16x16.png';
+//     const appleTouchIconUrl = mainImage
+//       ? `${mainImage.asset.url}`
+//       : '/apple-touch-icon.png';
+
+//     return {
+//       title: pageTitle,
+//       description: metaDescription,
+//       openGraph: {
+//         title: pageTitle,
+//         description: metaDescription,
+//         images: [metaImage],
+//       },
+//       icons: {
+//         icon: faviconUrl,
+//         shortcut: shortcutIconUrl,
+//         apple: appleTouchIconUrl,
+//       },
+//     };
+//   } else {
+//     return {
+//       title: 'Item Not Found',
+//       openGraph: {
+//         title: 'Item Not Found',
+//         description: 'No item found',
+//         images: ['/fallback-image.jpg'],
+//       },
+//       icons: {
+//         icon: '/favicon.ico',
+//         shortcut: '/favicon-16x16.png',
+//         apple: '/apple-touch-icon.png',
+//       },
+//     };
+//   }
+// };
+
+// export default metadata;
