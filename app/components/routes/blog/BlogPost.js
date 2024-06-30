5a3ffb1103d763c018f188cf3da3dbc4c5668276
Scottsdaaale <scottsdaaale@gmail.com>
2024-06-30 14:55:48 -0400

    copy button works as expected

--- app/components/routes/blog/BlogPost.js
+++ app/components/routes/blog/BlogPost.js
@@ -1,4 +1,3 @@
-
 'use client';
 
 import React from 'react';
@@ -19,23 +18,32 @@ function BlogPost({ params }) {
   const [post, setPost] = React.useState(null);
   const [error, setError] = React.useState(null);
 
-React.useEffect(() => {
-  const fetchPost = async () => {
-    if (params && params.slug) {
-      const query = `*[_type == "post" && slug.current == $slug][0]{ title, mainImage { asset->{ id, url }, alt }, body }`;
-      const queryParams = { slug: params.slug };
+  React.useEffect(() => {
+    const fetchPost = async () => {
+      if (params && params.slug) {
+        const query = `*[_type == "post" && slug.current == $slug][0]{ title, mainImage { asset->{ id, url }, alt }, body }`;
+        const queryParams = { slug: params.slug };
 
-      try {
-        const post = await client.fetch(query, queryParams);
-        setPost(post);
-      } catch (error) {
-        console.error('Sanity fetch error:', error);
-        setError(error);
+        try {
+          const post = await client.fetch(query, queryParams);
+          setPost(post);
+        } catch (error) {
+          console.error('Sanity fetch error:', error);
+          setError(error);
+        }
       }
-    }
+    };
+    fetchPost();
+  }, [params]);
+
+  const copyToClipboard = (text) => {
+    navigator.clipboard.writeText(text).then(() => {
+      console.log('Copied to clipboard');
+      // You could add a state here to show a "Copied!" message
+    }).catch(err => {
+      console.error('Failed to copy: ', err);
+    });
   };
-  fetchPost();
-}, [params]);
 
   if (error) {
     return <div>Error: {error.message}</div>;
@@ -57,14 +65,12 @@ React.useEffect(() => {
       )
   );
 
-  // Error checking and fallbacks for mainImage and title
   const mainImageUrl =
     post.mainImage && post.mainImage.asset && post.mainImage.asset.url;
   const mainImageAlt = (post.mainImage && post.mainImage.alt) || '';
   const title = post.title || '';
 
   const myPortableTextComponents = {
-    // types: {},
     marks: {
       Color: ({ value, children }) => (
         <span className='textRed'>{children}</span>
@@ -83,44 +89,44 @@ React.useEffect(() => {
         </a>
       ),
       code: ({ children }) => {
-  const renderCodeContent = (content) => {
-    return content
-      .map((child) => {
-        if (typeof child === 'string') {
-          return child;
-        } else if (React.isValidElement(child)) {
-          if (child.type === 'br') {
-            return '\n';
-          } else if (child.props.children) {
-            return renderCodeContent(React.Children.toArray(child.props.children));
-          } else {
-            return '';
-          }
-        } else {
-          return '';
-        }
-      })
-      .join('');
-  };
+        const renderCodeContent = (content) => {
+          return content
+            .map((child) => {
+              if (typeof child === 'string') {
+                return child;
+              } else if (React.isValidElement(child)) {
+                if (child.type === 'br') {
+                  return '\n';
+                } else if (child.props.children) {
+                  return renderCodeContent(React.Children.toArray(child.props.children));
+                } else {
+                  return '';
+                }
+              } else {
+                return '';
+              }
+            })
+            .join('');
+        };
 
-  const codeString = renderCodeContent(React.Children.toArray(children));
+        const codeString = renderCodeContent(React.Children.toArray(children));
 
-  return (
-    <div className='flex flex-col bg-[#313131]'>
-      <div className='flex justify-end m-2'>
-        <button
-          className='text-xs text-[#ececec]'
-          onClick={() => copyToClipboard(codeString)}
-        >
-          Copy
-        </button>
-      </div>
-      <pre className='bg-[#070707] p-4 overflow-x-auto text-[#ececec] whitespace-pre-wrap break-words'>
-        <code>{children}</code>
-      </pre>
-    </div>
-  );
-},
+        return (
+          <div className='flex flex-col bg-[#313131]'>
+            <div className='flex justify-end m-2'>
+              <button
+                className='text-xs text-[#ececec]'
+                onClick={() => copyToClipboard(codeString)}
+              >
+                Copy
+              </button>
+            </div>
+            <pre className='bg-[#070707] p-4 overflow-x-auto text-[#ececec] whitespace-pre-wrap break-words'>
+              <code>{children}</code>
+            </pre>
+          </div>
+        );
+      },
       image: ({ value }) => {
         if (!value.asset) {
           return null;
